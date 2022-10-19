189f2da981e88ca01cac6640d18f133fba2f0384
心碎男妈妈 <txt_ch@163.com>
2022-10-19 08:25:39 +0000

    add WG.set_value and WG.get_value

--- wsmud_pluginss.user.js
+++ wsmud_pluginss.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         wsmud_pluginss
 // @namespace    cqv1
-// @version      0.0.32.277
+// @version      0.0.32.278
 // @date         01/07/2018
-// @modified     29/09/2022
+// @modified     19/10/2022
 // @homepage     https://greasyfork.org/zh-CN/scripts/371372
 // @description  武神传说 MUD 武神脚本 武神传说 脚本 qq群367657589
 // @author       fjcqv(源程序) & zhzhwcn(提供websocket监听)& knva(做了一些微小的贡献) &Bob.cn(raid.js作者)
@@ -1437,6 +1437,12 @@
                 }
             }
             return false;
+        },
+        set_value(key,value){
+            return GM_setValue(key,value);
+        },
+        get_value(key){
+            return GM_getValue(key);
         },
         login: function () {
             role = $('.role-list .select').text().split(/[\s\n]/).pop();
