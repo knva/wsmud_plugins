25a0cfa5d93b4ba2855443072c89e276d53faf7a
心碎男妈妈 <txt_ch@163.com>
2024-07-09 02:00:20 +0000

    Update. version code

--- wsmud_pluginss.user.js
+++ wsmud_pluginss.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         wsmud_pluginss
 // @namespace    cqv1
-// @version      0.0.32.298
+// @version      0.0.32.299
 // @date         01/07/2018
-// @modified     07/03/2024
+// @modified     07/09/2024
 // @homepage     https://greasyfork.org/zh-CN/scripts/371372
 // @description  武神传说 MUD 武神脚本 武神传说 脚本 qq群367657589
 // @author       fjcqv(源程序) & zhzhwcn(提供websocket监听)& knva(做了一些微小的贡献) &Bob.cn(raid.js作者)
